ed6fd184fe98b02ca955d3e27d4bbf0d408b4af0
CuervoEv <juliancuervomartinez@gmail.com>
2026-03-16 19:17:45 -0500

    Agreg

--- database.py
+++ database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# URL DIRECTA - como en la guía página 4
-DB_URL = f"mysql://root:IomHTqBUYCZfUQnlmbFwsNGXxXXAmTfx@centerbeam.proxy.rlwy.net:13371/railway"
+
+DB_URL = f"mysql+pymysql://root:IomHTqBUYCZfUQnlmbFwsNGXxXXAmTfx@centerbeam.proxy.rlwy.net:13371/railway"
 
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
